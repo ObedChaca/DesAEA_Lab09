1e0f8707f80ca0173aab855a2a6a7cf5daed11dc
Obed Chaca <obed.chaca@tecsup.edu.pe>
2021-10-16 23:59:18 -0500

    Agregar archivos de proyecto.

---
+++ WebApplication1/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WebApplication1.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+
+}
